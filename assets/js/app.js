35ba0e40b6f22c2a889ea7ee622bb761db8a01e5
Le Phuoc Thang Nguyen <thang@fromttm.com>
2016-03-29 14:33:35 -0700

    1

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ThangTheMan on 3/29/16.
+ */
+
+var width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
+if (width >= 600) {
+    document.write("<p class='info'> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>");
+}
+else {
+    document.write("<p class='info'> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>");
+
+}
