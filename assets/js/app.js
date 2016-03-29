fd43eeee7000b5baba927ec4870d0e9682f60eeb
Le Phuoc Thang Nguyen <thang@fromttm.com>
2016-03-29 15:05:38 -0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,10 @@
  */
 
 var width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
-if (width >= 600) {
+if (width >= 769) {
     document.write("<p class='info'> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>");
 }
 else {
-    document.write("<p class='info'> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>");
+    document.write("");
 
 }
